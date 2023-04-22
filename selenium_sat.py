2540e8a414efe66942eeed18aa6eaed8e6608ed7
Jakub <j.kostansky@gmail.com>
2023-04-22 14:34:55 +0200

    commit

--- selenium_sat.py
+++ selenium_sat.py
@@ -1,5 +1,22 @@
+import unittest
 from selenium import webdriver
 import datetime
 
-przegladarka = webdriver.Chrome()
+#zmienna globalna (moze byc uzyta w kazdym przypadku
+#driver = webdriver.Chrome()
 
+class RegistrationTests(unittest.TestCase):
+    def setUp(self):
+        #przygotowanie testu (warunki wstępne0
+        #1. Otwarta strona glowna
+        #1a) tworze instancje klasy Chrome()
+        self.driver = webdriver.Chrome()    # zmienna lokalna z uzycie self
+        # maksymalizacja okna
+        self.driver.maximize_window()
+        # 1b) otwieram strone glowna
+        self.driver.get("https://www.eobuwie.com.pl/")
+
+
+
+    def testNoNameEntered(self):
+        pass
